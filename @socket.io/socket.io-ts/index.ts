bdfd3ad582fe9298996c89e10d3d1f4afa64f0db
changbaihe <changbaihe@donews.com>
2020-06-30 17:24:43 +0800

    refactor(): . adjust directory

--- @socket.io/socket.io-ts/index.ts
+++ @socket.io/socket.io-ts/index.ts
@@ -1,8 +1,9 @@
 import * as socketio from 'socket.io';
 import * as http from 'http';
 const server = http.createServer();
+console.log(typeof server)
 const io = socketio(server);
-io.on('connection', client => {
-
+io.on('connection', socket => {
+    socket.join(socket.id);
 });
 server.listen(3000, () => console.log('listening: 3000...'));
