88481abcdbe318aabedc880a5fd42329fb4f71e5
changbaihe <changbaihe@donews.com>
2020-07-13 19:21:01 +0800

    update

--- Typescript/queue/queue.demo.ts
+++ Typescript/queue/queue.demo.ts
@@ -43,16 +43,20 @@ function test2() {
 //   console.log(a);
 // })
 
-class RoomContainerRegistry extends Map<string, string> {
+class RoomContainerRegistry extends Map<string, any> {
   get(k: any) {
     return super.get(`${k}`);
   }
-  set(k: any, v: string) {
+  set(k: any, v: any) {
     return super.set(`${k}`, v);
+  }
+  delete(k: any) {
+    return super.delete(`${k}`);
   }
 }
 
 let a = new RoomContainerRegistry();
 a.set(1, '20');
 a.set(2, '50');
-console.log([...a.keys()], a.get(1), a.get(2))
+console.log([...a.keys()], a.get(1), a.get(2));
+console.log(a.delete(1), [...a.keys()])
