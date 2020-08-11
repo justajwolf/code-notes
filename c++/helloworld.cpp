ce9dab5f950464b2e7a3cacbcd733fa1e48d2452
changbaihe <changbaihenx@163.com>
2020-08-11 13:31:28 +0800

    update

---
+++ c++/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+  vector<string> msg {"hello", "c++", "world", "from", "VS Code", "and the C++ extension!"};
+  for (const string& word : msg) {  
+    cout << word << " ";
+  }
+  cout << endl;
+}
