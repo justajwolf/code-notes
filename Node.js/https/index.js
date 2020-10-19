ac0395ce58ea865fb02dc95182d112a82c577f85
CBH <baihe.chang@17zuoye.com>
2020-10-19 11:54:10 +0800

    feat(system/nodejs):添加给文件权限命令,补充nodejs http/https使用

--- Node.js/https/index.js
+++ Node.js/https/index.js
@@ -1,27 +1,45 @@
 const https = require('https');
-const req = https.request(
-  {
-    hostname: 'raw.githubusercontent.com',
-    port: 443,
-    path: '/uiwjs/province-city-china/master/dist/level.json',
-    method: 'GET',
-  },
-  res => {
+const test1 = () => {
+  const req = https.request(
+    {
+      hostname: 'raw.githubusercontent.com',
+      port: 443,
+      path: '/uiwjs/province-city-china/master/dist/level.json',
+      method: 'GET',
+    },
+    res => {
+      console.log(res.statusCode, res.headers);
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        console.log(data);
+      })
+    },
+  ).on('error', console.error);
+  req.end();
+}
+const test2 = () => {
+  https.get('https://raw.githubusercontent.com/uiwjs/province-city-china/master/dist/level.json', res => {
     console.log(res.statusCode, res.headers);
     let data = '';
     res.on('data', chunk => data += chunk);
     res.on('end', () => {
       console.log(data);
-    })
-  },
-).on('error', console.error);
-req.end();
+    });
+  }).on('error', console.error);
+}
 
-// https.get('https://raw.githubusercontent.com/uiwjs/province-city-china/master/dist/level.json', res => {
-//   console.log(res.statusCode, res.headers);
-//   let data = '';
-//   res.on('data', chunk => data += chunk);
-//   res.on('end', () => {
-//     console.log(data);
-//   });
-// }).on('error', console.error);
+const http = require('http');
+http.createServer((req, res) => {
+  https.get('https://ea5765b0c8415a76e7dc6503099ed150.dlied1.cdntips.net/dlied1.qq.com/qqweb/PCQQ/PCQQ_EXE/PCQQ2020.exe?mkey=5f815d392be3dac7&f=8507&cip=43.227.252.50&proto=https&access_type=$header_ApolloNet', r => {
+    console.log(r.statusCode, r.headers);
+    let data = '';
+    // res.on('data', chunk => data += chunk);
+    // res.on('end', () => {
+    //   console.log(data);
+    // });
+    res.setHeader('Content-Disposition', 'attachment; filename="test.exe"');
+    res.setHeader('content-type', r.headers['content-type']);
+    r.pipe(res);
+  }).on('error', console.error);
+}).listen(3000, () => console.log('ok'));
--- system/linux-mac/shell.md
+++ system/linux-mac/shell.md
@@ -3,3 +3,5 @@
     >$ cat /proc/version 
 * (Linux/Mac)给[user]加[folder]操作权限
     >$ sudo chown -R [user] [folder]
+* (Linux/Mac)给[file]加操作权限
+    >$ sudo chown -R [权限代码] [文件名]
