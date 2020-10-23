47cbd68446d0190b22cb6cd371ba5b28176c8b64
CBH <baihe.chang@17zuoye.com>
2020-10-23 21:42:26 +0800

    update:c++debug

--- c++/helloworld.cpp
+++ c++/helloworld.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-  vector<string> msg {"hello", "c++", "world", "from", "VS Code", "and the C++ extension!"};
+  vector<string> msg = {"hello", "c++", "world", "from", "VS Code", "and the C++ extension!"};
   for (const string& word : msg) {  
     cout << word << " ";
   }
