968cf28c3441fbcd7e26314221ab0ebb8ad33e78
changbaihe <changbaihenx@163.com>
2020-11-10 00:57:23 +0800

    update(electron):调整gulp

--- electron.js/vip-admin/app/gulpfile.js
+++ electron.js/vip-admin/app/gulpfile.js
@@ -5,43 +5,28 @@ const gulp = require('gulp');
 
 // 清除旧的构建数据
 gulp.task(
-  // 'all:clean', async cb => {
-  //   await Promise.all([
-  //     rimraf('../pages/dist/**/*'),
-  //     rimraf('../server/dist/**/*'),
-  //     rimraf('./pages'),
-  //     rimraf('./server'),
-  //     rimraf('./out'),
-  //   ]).then(cb)
-  // },
   'all:clean', async cb => {
-    await rimraf('../pages/dist/**/*');
-    await rimraf('../server/dist/**/*');
-    await rimraf('./pages');
-    await rimraf('./server');
-    await rimraf('./out');
-    cb();
+    [
+      '../pages/dist',
+      '../server/dist',
+      './pages',
+      './server',
+      './out',
+    ].reduce((promise, dir) => {
+      return promise && (promise = promise.then(() => rimraf(dir)).catch(console.error));
+    }, Promise.resolve()).then(cb);
   },
 );
 
 // 构建pages和server
-gulp.task('pages:build', async cb => await exec('cd ../pages && npm run build').then(cb));
-gulp.task('pages:copy', async (cb) => gulp.src('../pages/dist/**/*').pipe(gulp.dest('./pages')).end(cb));
-gulp.task('server:build', async cb => await exec('cd ../server && npm run build').then(cb));
-gulp.task('server:copy', async (cb) => gulp.src('../server/dist/**/*').pipe(gulp.dest('./server')).end(cb));
+gulp.task('pages:build', async cb => await exec('cd ../pages && npm run build') && cb());
+gulp.task('server:build', async cb => await exec('cd ../server && npm run build') && cb());
+gulp.task('pages:copy', () => gulp.src('../pages/dist/**/*').pipe(gulp.dest('./pages')));
+gulp.task('server:copy', () => gulp.src('../server/dist/**/*').pipe(gulp.dest('./server')));
 gulp.task(
   'build', gulp.series.apply(gulp, [
     'all:clean',
     gulp.parallel(['pages:build', 'server:build']),
     gulp.parallel(['pages:copy', 'server:copy']),
-  ]),
-);
-
-// 打包成桌面程序包
-gulp.task('package:exec', async cb => await exec('electron-forge package').then(cb));
-gulp.task(
-  'package', gulp.series.apply(gulp, [
-    'build',
-    'package:exec',
   ]),
 );
