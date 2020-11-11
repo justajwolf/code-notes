d1ec92a3fb983b2dfe233d2cb587e3efa0106984
CBH <baihe.chang@17zuoye.com>
2020-11-11 20:33:24 +0800

    update(electron.js):pages add nav

--- electron.js/vip-admin/pages/src/App.vue
+++ electron.js/vip-admin/pages/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <router-view></router-view>
   </div>
 </template>
 
@@ -17,7 +13,7 @@
   color: #2c3e50;
 }
 
-#nav {
+#home {
   padding: 30px;
 
   a {
--- electron.js/vip-admin/pages/src/views/Home.vue
+++ electron.js/vip-admin/pages/src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="20%">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import Nav from '@/components/Nav.vue';
+// import Main from '@/components/Main.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // Nav,
+    // Main
   }
 };
 </script>
+
+<style lang="less" scoped>
+div {
+  margin-left: 10%;
+  margin-right: 10%;
+}
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
