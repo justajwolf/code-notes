68bb1371b9d8ba51a6ed39bc8c28e8598103bd64
CBH <changbaihenx@163.com>
2020-11-22 23:08:57 +0800

    update(go):init

---
+++ go/entry_level/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("hello, world")
+	fmt.Println("The time is ", time.Now())
+}
---
+++ go/entry_level/index.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "changbaihe"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d int
+	fmt.Println(d)
+
+	var e bool
+	fmt.Println(e)
+
+	f := "lvrui"
+	fmt.Println(f)
+}
