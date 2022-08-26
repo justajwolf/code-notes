793a1f89130ff6e857c048f802d5ac2e922f6edd
justajwolf <changbaihenx@163.com>
2022-08-26 15:42:55 +0800

    update(langs/js): add test demo

--- langs/js/iteration_protocol.js
+++ langs/js/iteration_protocol.js
@@ -18,8 +18,34 @@ const a = {
         }
     }
 };
-
-console.log(Array.from([...a]));
+const b = [4, 5, 6];
+console.log(Array.from([...b]));
 for (const iterator of a) {
     console.log(iterator);
 }
+
+const test = Object.getOwnPropertySymbols(a);
+const test2 = Object.getOwnPropertySymbols(b);
+console.log(test);
+
+
+const s1 = Symbol('1');
+const s2 = Symbol.for('1');
+
+console.log(Symbol.keyFor(s1));
+console.log(Symbol.keyFor(s2));
+const x = new b.constructor[Symbol.species];
+console.log();
+
+
+class test3 {
+    static get [Symbol.species]() { return test3 }
+    constructor() {
+        console.log(new Date().toISOString().slice(0, -1).replace("T", " "))
+        this.timestamps = Date.now();
+    }
+}
+
+const t3 = new test3;
+const t4 = new t3.constructor[Symbol.species];
+console.log(t3, t4)
--- langs/js/npm/lodash/index.ts
+++ langs/js/npm/lodash/index.ts
@@ -1,4 +1,6 @@
 import * as _ from "lodash";
 
-const result = _.groupBy([], x => x);
+const result1 = _.groupBy([], x => x);
+
+const result2 = _.pick({a:1, b:2, c:3}, []);
 console.log();
