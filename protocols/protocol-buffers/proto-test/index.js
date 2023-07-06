0bb74fb4c1ff4fe012c96c1b4d2ef045ad679100
justajwolf <changbaihenx@163.com>
2023-07-06 15:23:04 +0800

    update(protocols/……/proto-test): 补充grpc的any类型，浅看any长啥样

--- protocols/protocol-buffers/proto-test/index.js
+++ protocols/protocol-buffers/proto-test/index.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const pbCompiler = require('google-protobuf');
 const pb = require('protobufjs');
-// const root = pb.loadSync(path.join(__dirname, './detail.proto'));
+const root = pb.loadSync(path.join(__dirname, './detail.proto'));
+console.log(JSON.stringify(root.toJSON(), null, 2));
+
 const pb2 = require('@grpc/proto-loader');
-const root = pb2.loadSync(path.join(__dirname, './detail.proto'));
-console.log(JSON.stringify(root.toJSON(), null, ' '));
+const root2 = pb2.loadSync(path.join(__dirname, './detail.proto'));
+console.log(Object.keys(root2));
