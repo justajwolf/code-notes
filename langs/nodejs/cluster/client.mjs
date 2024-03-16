bd98a6b4dffa8c9c96a9e3b92007851545de6e2a
justajwolf <changbaihenx@163.com>
2024-03-16 20:25:43 +0800

    feat(langs/nodejs): update cluster/client.mjs

--- langs/nodejs/cluster/client.mjs
+++ langs/nodejs/cluster/client.mjs
@@ -9,13 +9,29 @@ export function clientSendMock(request = async () => { }) {
 
 export async function curlRequest(url = "http://127.0.0.1:8080") {
     const res = spawnSync(`curl ${url}`, { shell: true, encoding: "utf8" });
-    console.log(res.stdout);
+    const content = res.stdout.trim() || res.stderr.trim() || `${res.pid},${res.status}`;
+    console.log(content);
 }
 
-export async function httpRequest(url = "http://127.0.0.1:8080") {
-    const res = await fetch(url);
-    const text = await res.text();
-    console.log(text)
+export async function httpRequest(url = "http://127.0.0.1:8080", type = "fetch") {
+    switch (type) {
+        case "fetch": {
+            // 这种方式，每次请求连接总是在同一个进程上处理，后面有时间了，看看这个具体原因吧，fetch底层实现可能有连接优化。
+            const res = await fetch(url);
+            const text = await res.text();
+            console.log(text)
+            return;
+        }
+        case "spawn":
+        default: {
+            // 这种方式，在cluster模式下，使用spawnSync，竟然没反应。。。后面有时间了，仔细看看啥原因吧，估计cluster模式对所有子进程都有拦截吧。
+            const cmd = `await fetch('${url}').then(async (res) => console.log(await res.text()));`
+            const res = spawnSync(`node --input-type module -e "${cmd}"`, { shell: true, encoding: "utf8" });
+            const content = res.stdout.trim() || res.stderr.trim() || `${res.pid},${res.status}`;
+            console.log(content);
+            return;
+        }
+    }
 }
 
 if (import.meta.filename.endsWith(process.argv[1])) {
@@ -25,6 +41,6 @@ if (import.meta.filename.endsWith(process.argv[1])) {
             clientSendMock(curlRequest.bind(null, url))
             break;
         default:
-            clientSendMock(httpRequest.bind(null, url))
+            clientSendMock(httpRequest.bind(null, url, "spawn"))
     }
 }
