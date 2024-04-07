69f4b78a2d82ab02124d3da9fd745bf953662482
justajwolf <changbaihenx@163.com>
2024-04-07 09:32:33 +0800

    chore(langs/go): update contents

--- go-dev/ac-code/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/justajwolf/go-dev/ac-code
-
-go 1.16
-
-replace github.com/justajwolf/go-dev/ac-code => ./
--- langs/go/web-service-gin/main.go
+++ langs/go/web-service-gin/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/justajwolf/go-dev/go-basic/attempt"
 	"github.com/justajwolf/go-dev/web-service-gin/albums"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
